abb8e394cb7e97086bf78897bc91b27365f09679
paulogilvan <paulogilvan74@gmail.com>
2024-03-25 19:40:32 -0300

    modified css responsive

--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
     display: flex;
     justify-content: center;
+    background-color: #CCC;
+    height: 100vh;
 `;
 
 export const Container = styled.div`
@@ -13,6 +15,18 @@ export const Container = styled.div`
     height: 90vh;
     margin-top: 50px;
     border-radius: 30px;
+
+    @media screen and (width:1024px) and (max-width: 1920px) {
+        width: 40%;
+    }
+
+    @media(width <= 768px) {
+        width: 50%;
+    } 
+
+    @media(max-width: 425px) {
+        width: 80%;
+    }
 `;
 
 export const Logo = styled.img`
@@ -36,11 +50,25 @@ export const AreaInput = styled.div`
         border: 2px solid #1e6e36;
         width: 120%;
         font-size: 20px;
+        color: #63384a;
         background-color: transparent;  
         box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
         
         &::placeholder {
             color: #63384a;
+        }
+
+        @media(width >= 1024px) {
+            width: 85%;
+            margin-top: 25%;
+        }
+
+        @media(width: 768px) {
+            width: 75%;
+        } 
+
+        @media(max-width: 425px) {
+            width: 65%;
         }
     }
 
@@ -51,17 +79,42 @@ export const AreaInput = styled.div`
         border: 2px solid #1e6e36;
         width: 120%;
         font-size: 20px;
+        color: #63384a;
         background-color: transparent;
         box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 
         &::placeholder {
             color: #63384a;
         }
+
+        @media(width >= 1024px) {
+            width: 85%;
+        }
+
+        @media(width: 768px) {
+            width: 75%;
+        } 
+
+        @media(max-width: 425px) {
+            width: 65%;
+        }
     }
 
     p {
         color: #63384a;
         cursor: pointer;
+    }
+
+    @media(width >= 1024px) {
+        margin-top: 10%;
+    } 
+
+    @media(width: 768px) {
+        margin-top: 30%;
+    } 
+
+    @media(max-width: 425px) {
+        margin-top: 55%;
     }
 `;
 
